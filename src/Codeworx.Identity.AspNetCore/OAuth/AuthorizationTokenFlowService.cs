a9ed37d8fea72a6843f97579a9052e4f731b0ed4
Andy Nöltner <andy.noeltner@dakanet.de>
2019-05-13 15:27:43 +0200

    Handle unregistered client

--- src/Codeworx.Identity.AspNetCore/OAuth/AuthorizationTokenFlowService.cs
+++ src/Codeworx.Identity.AspNetCore/OAuth/AuthorizationTokenFlowService.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Codeworx.Identity.OAuth;
+using Codeworx.Identity.OAuth.Authorization;
 
 namespace Codeworx.Identity.AspNetCore.OAuth
 {
     public class AuthorizationTokenFlowService: IAuthorizationFlowService
     {
+        private readonly IOAuthClientService _oAuthClientService;
+
         public string SupportedAuthorizationResponseType => Identity.OAuth.Constants.ResponseType.Token;
 
-        public Task<IAuthorizationResult> AuthorizeRequest(AuthorizationRequest request, string currentTenantIdentifier)
+        public AuthorizationTokenFlowService(IOAuthClientService oAuthClientService)
         {
+            _oAuthClientService = oAuthClientService;
+        }
+
+        public async Task<IAuthorizationResult> AuthorizeRequest(AuthorizationRequest request, string currentTenantIdentifier)
+        {
+            var clientRegistrations = await _oAuthClientService.GetForTenantByIdentifier(currentTenantIdentifier);
+
+            if (clientRegistrations.Any(p => p.Identifier == request.ClientId && p.SupportedOAuthMode == request.ResponseType) == false)
+            {
+                return new UnauthorizedClientResult(request.State, request.RedirectUri);
+            }
+
             throw new NotImplementedException();
         }
     }
---
+++ tests/Codeworx.Identity.Test/AspNetCore/OAuth/AuthorizationTokenFlowServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Codeworx.Identity.AspNetCore.OAuth;
+using Codeworx.Identity.Model;
+using Codeworx.Identity.OAuth;
+using Codeworx.Identity.OAuth.Authorization;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Codeworx.Identity.Test.AspNetCore.OAuth
+{
+    public class AuthorizationTokenFlowServiceTests
+    {
+        [Fact]
+        public async Task AuthorizeRequest_ClientNotRegistered_ReturnsError()
+        {
+            var request = new AuthorizationRequestBuilder().Build();
+            var oAuthClientServiceStub = new Mock<IOAuthClientService>();
+
+            var instance = new AuthorizationTokenFlowService(oAuthClientServiceStub.Object);
+
+            var result = await instance.AuthorizeRequest(request, string.Empty);
+
+            Assert.IsType<UnauthorizedClientResult>(result);
+        }
+    }
+}
