5a71d168279cd2e6d86252ff85db07239d17de14
Andy Nöltner <andy.noeltner@dakanet.de>
2019-05-13 14:50:31 +0200

    Create data classes for implicit grant

---
+++ src/Codeworx.Identity.AspNetCore/OAuth/AuthorizationTokenOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Codeworx.Identity.AspNetCore.OAuth
+{
+    public class AuthorizationTokenOptions
+    {
+        public int Length { get; set; } = 10;
+
+        public double ExpirationInSeconds { get; set; } = TimeSpan.FromMinutes(10).TotalSeconds;
+    }
+}
