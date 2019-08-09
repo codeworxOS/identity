ed5c3c55065dccc9632708014b3ef8b1c078885e
Andy Nöltner <andy.noeltner@dakanet.de>
2019-08-09 12:19:09 +0200

    Implemt logic for tenant selection

---
+++ src/Codeworx.Identity.Primitives/Model/TenantSelectionRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Codeworx.Identity.Model
+{
+    [DataContract]
+    public class TenantSelectionRequest
+    {
+        [DataMember(Order = 1, Name = "tenant")]
+        public string Tenant { get; set; }
+    }
+}
