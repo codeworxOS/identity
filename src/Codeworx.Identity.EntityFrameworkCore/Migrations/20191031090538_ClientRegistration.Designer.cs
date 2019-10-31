5db716a33b130534f70bfc594202e5ab3b0738c2
Andy Nöltner <andy.noeltner@dakanet.de>
2019-10-31 10:08:05 +0100

    Add migration for client registration

--- src/Codeworx.Identity.EntityFrameworkCore/Migrations/20191031090538_ClientRegistration.Designer.cs
+++ src/Codeworx.Identity.EntityFrameworkCore/Migrations/20191031090538_ClientRegistration.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Codeworx.Identity.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(CodeworxIdentityDbContext))]
-    partial class CodeworxIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191031090538_ClientRegistration")]
+    partial class ClientRegistration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Codeworx.Identity.EntityFrameworkCore/Migrations/20191031090538_ClientRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Codeworx.Identity.EntityFrameworkCore.Migrations
+{
+    public partial class ClientRegistration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientConfiguration",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ClientSecret = table.Column<byte[]>(nullable: true),
+                    ClientSecretSalt = table.Column<byte[]>(nullable: true),
+                    DefaultRedirectUri = table.Column<string>(nullable: true),
+                    FlowTypes = table.Column<int>(nullable: false),
+                    TokenExpiration = table.Column<TimeSpan>(nullable: false),
+                    ValidRedirectUrls = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientConfiguration", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientConfiguration");
+        }
+    }
+}
