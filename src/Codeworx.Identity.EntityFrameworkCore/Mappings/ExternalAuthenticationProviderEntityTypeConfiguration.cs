fc1c9fb917a4bb8e0e5aed019fd4026cb2ddf63e
Andy Nöltner <andy.noeltner@dakanet.de>
2019-10-31 12:04:22 +0100

    Add link entity from authentication providers to users

---
+++ src/Codeworx.Identity.EntityFrameworkCore/Mappings/ExternalAuthenticationProviderEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Codeworx.Identity.EntityFrameworkCore.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Codeworx.Identity.EntityFrameworkCore.Mappings
+{
+    public class ExternalAuthenticationProviderEntityTypeConfiguration : IEntityTypeConfiguration<ExternalAuthenticationProvider>
+    {
+        public void Configure(EntityTypeBuilder<ExternalAuthenticationProvider> builder)
+        {
+            builder.ToTable("ExternalAuthenticationProvider");
+
+            builder.Ignore(p => p.Filter);
+        }
+    }
+}
---
+++ src/Codeworx.Identity.EntityFrameworkCore/Model/AuthenticationProviderUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codeworx.Identity.EntityFrameworkCore.Model
+{
+    public class AuthenticationProviderUser
+    {
+        public Guid RightHolderId { get; set; }
+
+        public User User { get; set; }
+
+        public Guid ProviderId { get; set; }
+
+        public ExternalAuthenticationProvider Provider { get; set; }
+
+        public string ExternalIdentifier { get; set; }
+    }
+}
