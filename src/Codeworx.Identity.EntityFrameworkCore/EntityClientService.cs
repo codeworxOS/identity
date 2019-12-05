7d5dae00765168b62ad25ecfb36be7623d2241e3
Andy Nöltner <andy.noeltner@dakanet.de>
2019-12-05 15:53:08 +0100

    Include missing redirect urls and scopes to in client registration

--- src/Codeworx.Identity.EntityFrameworkCore/EntityClientService.cs
+++ src/Codeworx.Identity.EntityFrameworkCore/EntityClientService.cs
@@ -20,7 +20,10 @@ namespace Codeworx.Identity.EntityFrameworkCore
         {
             var id = Guid.Parse(clientIdentifier);
 
-            return await _context.Set<ClientConfiguration>().SingleOrDefaultAsync(p => p.Id == id);
+            return await _context.Set<ClientConfiguration>()
+                .Include(p => p.ValidRedirectUrls)
+                .Include(p => p.AllowedScopes)
+                .SingleOrDefaultAsync(p => p.Id == id);
         }
     }
 }
