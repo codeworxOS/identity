1c246cb02844c53b3fcfbbb2e8a7d2ce3fa3adb3
Andy Nöltner <andy.noeltner@dakanet.de>
2020-03-25 14:13:56 +0100

    Separate constants for openid and oauth

--- src/Codeworx.Identity.Primitives/OAuth/Constants.cs
+++ src/Codeworx.Identity.Primitives/OAuth/Constants.cs
@@ -70,15 +70,6 @@
             public const string UnauthorizedClient = "unauthorized_client";
             public const string UnsupportedGrantType = "unsupported_grant_type";
             public const string UnsupportedResponseType = "unsupported_response_type";
-            public const string InteractionRequired = "interaction_required";
-            public const string LoginRequired = "login_required";
-            public const string AccountSelectionRequired = "account_selection_required";
-            public const string ConsentRequired = "consent_required";
-            public const string InvalidRequestUri = "invalid_request_uri";
-            public const string InvalidRequestObject = "invalid_request_object";
-            public const string RequestNotSupported = "request_not_supported";
-            public const string RequestUriNotSupported = "request_uri_not_supported";
-            public const string RegistrationNotSupported = "registration_not_supported";
         }
 
         public class GrantType
---
+++ src/Codeworx.Identity.Primitives/OpenId/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace Codeworx.Identity.OpenId
+{
+    public static class Constants
+    {
+        public class Error : Identity.OAuth.Constants.Error
+        {
+            public const string InteractionRequired = "interaction_required";
+            public const string LoginRequired = "login_required";
+            public const string AccountSelectionRequired = "account_selection_required";
+            public const string ConsentRequired = "consent_required";
+            public const string InvalidRequestUri = "invalid_request_uri";
+            public const string InvalidRequestObject = "invalid_request_object";
+            public const string RequestNotSupported = "request_not_supported";
+            public const string RequestUriNotSupported = "request_uri_not_supported";
+            public const string RegistrationNotSupported = "registration_not_supported";
+        }
+
+        public class ResponseType : Identity.OAuth.Constants.ResponseType
+        {
+            public const string IdToken = "id_token";
+        }
+    }
+}
