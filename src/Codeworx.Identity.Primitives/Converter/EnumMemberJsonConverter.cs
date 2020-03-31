9cc59f475309bccdb96b201c7a8397836ffa05a7
Andrea Steimel <ASteimel@lstelcom.com>
2020-03-31 15:37:55 +0200

    Add enum member converter for json

---
+++ src/Codeworx.Identity.Primitives/Converter/EnumMemberJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Codeworx.Identity.Converter
+{
+    public class EnumMemberJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var member = value.GetType().GetMember(value.ToString());
+            var enumMemberAttribute = member.SelectMany(p => p.GetCustomAttributes(typeof(EnumMemberAttribute), false))
+                .OfType<EnumMemberAttribute>()
+                .FirstOrDefault();
+
+            writer.WriteValue(enumMemberAttribute?.Value ?? value);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsEnum;
+        }
+    }
+}
---
+++ tests/Codeworx.Identity.Test/Cryptography/Converter/EnumMemberJsonConverterTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.Serialization;
+using Codeworx.Identity.Converter;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Codeworx.Identity.Test.Cryptography.Converter
+{
+    public class EnumMemberJsonConverterTests
+    {
+        private const string Part1Name = "Part1Name";
+        
+        private enum TestEnum
+        {
+            [EnumMember(Value = Part1Name)]
+            Part1,
+            Part2
+        }
+
+        [Fact]
+        public void CanConvert_Enum_ReturnsTrue()
+        {
+            var instance = new EnumMemberJsonConverter();
+
+            Assert.True(instance.CanConvert(typeof(TestEnum)));
+
+        }
+
+        [Fact]
+        public void CanConvert_Object_ReturnsFalse()
+        {
+            var instance = new EnumMemberJsonConverter();
+
+            Assert.False(instance.CanConvert(typeof(object)));
+        }
+
+        [Fact]
+        public void WriteJson_EnumWithAttribute_ReturnsCorrectSerialization()
+        {
+            var enumValue = TestEnum.Part1;
+            
+            var instance = new EnumMemberJsonConverter();
+            var jsonWriter = new Mock<JsonWriter>();
+            
+            instance.WriteJson(jsonWriter.Object, enumValue, null);
+
+            jsonWriter.Verify(p => p.WriteValue((object)Part1Name), Times.Once);
+        }
+
+        [Fact]
+        public void WriteJson_EnumWithoutAttribute_ReturnsEnumValue()
+        {
+            var enumValue = TestEnum.Part2;
+
+            var instance = new EnumMemberJsonConverter();
+            var jsonWriter = new Mock<JsonWriter>();
+
+            instance.WriteJson(jsonWriter.Object, enumValue, null);
+
+            jsonWriter.Verify(p => p.WriteValue(enumValue), Times.Once);
+        }
+    }
+}
