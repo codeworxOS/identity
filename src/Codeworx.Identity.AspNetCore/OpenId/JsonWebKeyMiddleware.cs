6968e7a0dfce6af6ed220b656f16a04dead66c3b
Andy Nöltner <andy.noeltner@dakanet.de>
2020-04-01 08:12:03 +0200

    JWK enpoint can display a list of keys

--- src/Codeworx.Identity.AspNetCore/OpenId/JsonWebKeyMiddleware.cs
+++ src/Codeworx.Identity.AspNetCore/OpenId/JsonWebKeyMiddleware.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
 using Codeworx.Identity.Cryptography;
+using Codeworx.Identity.OpenId.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
@@ -15,16 +19,29 @@ namespace Codeworx.Identity.AspNetCore.OpenId
             _next = next;
         }
 
-        public async Task Invoke(HttpContext context, IDefaultSigningKeyProvider signingKeyProvider)
+        public async Task Invoke(HttpContext context, IEnumerable<IDefaultSigningKeyProvider> keyProviders)
         {
-            var keyParameter = signingKeyProvider.GetKeyParameter();
+            var keys = new KeyList
+            {
+                Keys = keyProviders.Select(p => p.GetKeyParameter()).ToArray()
+            };
 
             context.Response.Headers.Add(HeaderNames.ContentType, "application/json;charset=utf-8");
 
-            var responseString = JsonConvert.SerializeObject(keyParameter);
+            var responseString = JsonConvert.SerializeObject(keys);
 
             await context.Response.WriteAsync(responseString)
                 .ConfigureAwait(false);
+        }
+
+        [DataContract]
+        public class KeyList
+        {
+            [DataMember(Name = "keys")]
+            public KeyParameter[] Keys
+            {
+                get; set;
+            }
         }
     }
 }
