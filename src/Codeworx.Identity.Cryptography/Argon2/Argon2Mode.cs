4f5a0685b141491f0831e8ca8c68b9f29c341200
Raphael Schwarz <raphael@codeworx.at>
2021-03-10 21:58:28 +0100

    Argon2 Library Update (strong-naming issues)

--- src/Codeworx.Identity.Cryptography/Argon2/Argon2Mode.cs
+++ src/Codeworx.Identity.Cryptography/Argon2/Argon2Mode.cs
@@ -1,8 +1,13 @@
-﻿namespace Codeworx.Identity.Cryptography.Argon2
+﻿using System;
+
+namespace Codeworx.Identity.Cryptography.Argon2
 {
+    [Flags]
     public enum Argon2Mode
     {
-        Argon2i = 0x00,
-        Argon2id = 0x01,
+        None = 0x00,
+        Argon2i = 0x01,
+        Argon2d = 0x02,
+        Argon2id = Argon2i | Argon2d,
     }
 }
