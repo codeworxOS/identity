5216d8715c6dfcca7719727715bb261d24d20acd
Stefanie Breuer <stefanie.breuer@insite-it.net>
2022-07-06 17:21:38 +0200

    create tests for login where mfa is already performed

---
+++ tests/Codeworx.Identity.Test/MFA/LoginWithFulfilledMfaTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Codeworx.Identity.Test.Provider;
+using NUnit.Framework;
+
+namespace Codeworx.Identity.Test.MFA
+{
+    public class LoginWithFulfilledMfaTests : MfaIntegrationTestBase
+    {
+        [Test]
+        public async Task LoginWithCodeFlow_MfaFulfilled_NoMfaRequired_DoesNotShowMfaAfterAuthorization()
+        {
+            this.ConfigureMfaTestUser(isMfaRequired: false, isMfaConfigured: true);
+            await this.Authenticate(TestConstants.Users.MfaTestUser.UserName, TestConstants.Users.MfaTestUser.Password);
+            await this.FulfillMfa();
+
+            var authorizationResponse = await this.GetAuthorizationResponse(TestConstants.Clients.DefaultTokenFlowClientId, TestConstants.Tenants.DefaultTenant.Id);
+
+            Assert.AreEqual(HttpStatusCode.Redirect, authorizationResponse.StatusCode);
+            Assert.AreEqual(this.GetRedirectUrl(), authorizationResponse.Headers.Location.GetLeftPart(System.UriPartial.Path));
+        }
+
+        [Test]
+        public async Task LoginWithCodeFlow_MfaFulfilled_MfaRequiredOnUser_DoesNotShowMfaAfterAuthorization()
+        {
+            this.ConfigureMfaTestUser(isMfaRequired: true, isMfaConfigured: true);
+            await this.Authenticate(TestConstants.Users.MfaTestUser.UserName, TestConstants.Users.MfaTestUser.Password);
+            await this.FulfillMfa();
+
+            var authorizationResponse = await this.GetAuthorizationResponse(TestConstants.Clients.DefaultTokenFlowClientId, TestConstants.Tenants.DefaultTenant.Id);
+
+            Assert.AreEqual(HttpStatusCode.Redirect, authorizationResponse.StatusCode);
+            Assert.AreEqual(this.GetRedirectUrl(), authorizationResponse.Headers.Location.GetLeftPart(System.UriPartial.Path));
+        }
+
+        [Test]
+        public async Task LoginWithCodeFlow_MfaFulfilled_MfaRequiredOnTenant_DoesNotShowMfaAfterAuthorization()
+        {
+            this.ConfigureMfaTestUser(isMfaRequired: false, isMfaConfigured: true);
+            await this.Authenticate(TestConstants.Users.MfaTestUser.UserName, TestConstants.Users.MfaTestUser.Password);
+            await this.FulfillMfa();
+
+            var authorizationResponse = await this.GetAuthorizationResponse(TestConstants.Clients.DefaultTokenFlowClientId, TestConstants.Tenants.MfaTenant.Id);
+
+            Assert.AreEqual(HttpStatusCode.Redirect, authorizationResponse.StatusCode);
+            Assert.AreEqual(this.GetRedirectUrl(), authorizationResponse.Headers.Location.GetLeftPart(System.UriPartial.Path));
+        }
+
+        [Test]
+        public async Task LoginWithCodeFlow_MfaFulfilled_MfaRequiredOnClient_DoesNotShowMfaAfterAuthorization()
+        {
+            this.ConfigureMfaTestUser(isMfaRequired: false, isMfaConfigured: true);
+            await this.Authenticate(TestConstants.Users.MfaTestUser.UserName, TestConstants.Users.MfaTestUser.Password);
+            await this.FulfillMfa();
+
+            var authorizationResponse = await this.GetAuthorizationResponse(TestConstants.Clients.MfaRequiredClientId, TestConstants.Tenants.DefaultTenant.Id);
+
+            Assert.AreEqual(HttpStatusCode.Redirect, authorizationResponse.StatusCode);
+            Assert.AreEqual(this.GetRedirectUrl(), authorizationResponse.Headers.Location.GetLeftPart(System.UriPartial.Path));
+        }
+    }
+}
