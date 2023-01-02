2df503bd302dd5e51c123a64c668080b2efc7768
Raphael Schwarz <raphael@codeworx.at>
2023-01-02 14:14:11 +0100

    persistenceMode

---
+++ src/Codeworx.Identity.Primitives/Configuration/FormsPersistenceMode.cs
@@ -0,0 +1,9 @@
+﻿namespace Codeworx.Identity.Configuration
+{
+    public enum FormsPersistenceMode
+    {
+        SessionWithPersistOption = 0x00,
+        SessionOnly = 0x01,
+        Persistent = 0x02,
+    }
+}
