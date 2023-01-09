e498e5b54a2e69d785c59c9525c883038009e5c8
Raphael Schwarz <raphael@codeworx.at>
2023-01-09 13:58:18 +0100

    fix scop registration

--- src/Codeworx.Identity.AspNetCore/Extensions/CodeworxIdentityAspNetCoreAspNetCoreExtensions.cs
+++ src/Codeworx.Identity.AspNetCore/Extensions/CodeworxIdentityAspNetCoreAspNetCoreExtensions.cs
@@ -399,7 +399,7 @@ namespace Codeworx.Identity.AspNetCore
             collection.AddSingleton<IAuthorizationCodeGenerator, AuthorizationCodeGenerator>();
             collection.AddTransient<IClientAuthenticationService, ClientAuthenticationService>();
             collection.AddSingleton<IDefaultSigningKeyProvider, DefaultSigningKeyProvider>();
-            collection.AddSingleton<ITokenProvider, JwtProvider>();
+            collection.AddTransient<ITokenProvider, JwtProvider>();
             collection.AddSingleton<IAuthorizationCodeCache, DistributedAuthorizationCodeCache>();
             collection.AddSingleton<IRefreshTokenCache, DistributedRefreshTokenCache>();
             collection.AddSingleton<IExternalTokenCache, DistributedExternalTokenCache>();
