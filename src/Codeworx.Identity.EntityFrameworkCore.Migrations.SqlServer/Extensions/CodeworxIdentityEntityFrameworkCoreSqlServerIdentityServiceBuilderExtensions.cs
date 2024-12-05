ff5156ce33def4ded53939234821b66115c52b51
Raphael Schwarz <raphael@codeworx.at>
2024-12-05 11:57:05 +0100

    Fix extension Method naming issue

--- src/Codeworx.Identity.EntityFrameworkCore.Migrations.SqlServer/Extensions/CodeworxIdentityEntityFrameworkCoreSqlServerIdentityServiceBuilderExtensions.cs
+++ src/Codeworx.Identity.EntityFrameworkCore.Migrations.SqlServer/Extensions/CodeworxIdentityEntityFrameworkCoreSqlServerIdentityServiceBuilderExtensions.cs
@@ -7,7 +7,19 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class CodeworxIdentityEntityFrameworkCoreSqlServerIdentityServiceBuilderExtensions
     {
+        [Obsolete("Use UseDbContestSqlServer method instead",true)]
         public static IIdentityServiceBuilder UseDbContextSqlite(this IIdentityServiceBuilder builder, Func<IServiceProvider, string> connectionStringFactory)
+        {
+            throw new NotSupportedException();
+        }
+
+        [Obsolete("Use UseDbContestSqlServer method instead", true)]
+        public static IIdentityServiceBuilder UseDbContextSqlite(this IIdentityServiceBuilder builder, string connectionString)
+        {
+            throw new NotSupportedException();
+        }
+
+        public static IIdentityServiceBuilder UseDbContextSqlServer(this IIdentityServiceBuilder builder, Func<IServiceProvider, string> connectionStringFactory)
         {
             builder.ServiceCollection.AddDbContext<CodeworxIdentityDbContext>(
                 (sp, builder) => builder.UseSqlServer(connectionStringFactory(sp), x => x.MigrationsAssembly(GetMigrationAssemblyName()))
@@ -16,7 +28,7 @@ namespace Microsoft.Extensions.DependencyInjection
             return builder.UseDbContext<CodeworxIdentityDbContext>();
         }
 
-        public static IIdentityServiceBuilder UseDbContextSqlite(this IIdentityServiceBuilder builder, string connectionString)
+        public static IIdentityServiceBuilder UseDbContextSqlServer(this IIdentityServiceBuilder builder, string connectionString)
         {
             builder.ServiceCollection.AddDbContext<CodeworxIdentityDbContext>(
                 (sp, builder) => builder.UseSqlServer(connectionString, x => x.MigrationsAssembly(GetMigrationAssemblyName()))
